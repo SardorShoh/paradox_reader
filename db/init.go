5a9e839d3a7b294b30fcb81c948754f174671753
SardorShoh <razzoqovsardor91@gmail.com>
2023-01-10 23:38:42 +0500

    multithread qo'shildi

--- db/init.go
+++ db/init.go
@@ -9,7 +9,7 @@ import (
 )
 
 func connect(path string) (*sql.DB, error) {
-	ole.CoInitializeEx(0, ole.COINIT_APARTMENTTHREADED|ole.COINIT_SPEED_OVER_MEMORY)
+	ole.CoInitializeEx(0, ole.COINIT_APARTMENTTHREADED|ole.COINIT_SPEED_OVER_MEMORY|ole.COINIT_MULTITHREADED)
 	// ole.CoInitialize(0)
 	dsn := fmt.Sprintf("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=%s;Extended Properties=Paradox 5.x;", path)
 	db, err := sql.Open("adodb", dsn)
